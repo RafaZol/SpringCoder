3e220726d7f7cacec42b961aa29678297c26884d
Rafael <zoletrafael101@gmail.com>
2025-08-27 15:43:29 -0300

    Creation of the Spring project to study the SpringBoot module of the Coder Java course

---
+++ src/main/java/br/com/rz/exerciciossd/ExerciciossdApplication.java
@@ -0,0 +1,13 @@
+package br.com.rz.exerciciossd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciciossdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciciossdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rz/exerciciossd/ExerciciossdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rz.exerciciossd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciossdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
